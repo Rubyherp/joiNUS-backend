9cfd99caaa23356eb03ee8ca33f7e4e0230edbe3
XN <horxiangneng123@gmail.com>
2026-05-21 17:06:14 +0800

    basic registration and login response

--- server.js
+++ server.js
@@ -1,13 +1,63 @@
 import express from 'express';
 import cors from 'cors';
+import { supabase } from './supabaseClient.js';
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
+// test connection with frontend
 app.get('/ping', (req, res) => {
     res.json({ message: 'pong from Node!' });
 });
 
+// user sign up
+// tested using postman first to see if it works
+app.post('/register', async (req, res) => {
+    const { email, password } = req.body;
+
+    const { data, error } = await supabase.auth.signUp({
+        email,
+        password
+    })
+
+    if (error) {
+        return res.status(400).json({ error: error.message });
+    }
+
+    return res.status(200).json({ message: 'User created successfully', user: data.user });
+
+})
+
+app.post('/login', async (req, res) => {
+    const { email, password } = req.body;
+
+    const { data, error } = await supabase.auth.signInWithPassword({
+        email,
+        password
+    })
+
+    if (error) {
+        return res.status(400).json({ error: error.message });
+    }
+
+    // jwt token for authorization not sure about the expiry tho =p
+    return res.status(200).json({
+        token: data.session.access_token,
+        user: data.user
+    })
+})
+
 app.listen(3000, () => console.log('Backend running on port 3000'));
+
+
+
+
+
+
+
+
+
+
