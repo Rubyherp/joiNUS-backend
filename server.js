55f3127c3878784f64261b5b2c17a0212378d82f
XN <horxiangneng123@gmail.com>
2026-05-24 02:36:32 +0800

    add logic to insert into profiles table for profile setup

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import cors from 'cors';
 import { supabase } from './supabaseClient.js';
+import authMiddleware from './middleware/authMiddleware.js';
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -21,7 +22,7 @@ app.post('/register', async (req, res) => {
 
     const { data, error } = await supabase.auth.signUp({
         email,
-        password
+        password,
     })
 
     if (error) {
@@ -44,11 +45,57 @@ app.post('/login', async (req, res) => {
         return res.status(400).json({ error: error.message });
     }
 
+    // check if user has a profile in db
+    const { data: profile, error: profileError } = await supabase
+        .from('profiles')
+        .select('id')
+        .eq('id', data.user.id)
+        .single();
+
+    if (profileError && profileError.code !== 'PGRST116') {
+        return res.status(500).json({ error: 'Failed to check profile' });
+    }
+
     // jwt token for authorization not sure about the expiry tho =p
     return res.status(200).json({
         token: data.session.access_token,
-        user: data.user
+        user: data.user,
+        hasProfile: !!profile
     })
+})
+
+app.post('/profileCreation', authMiddleware, async (req, res) => {
+    const {
+        username,
+        avatar,
+        major,
+        year,
+        contact,
+        about,
+        skills,
+        module,
+        experiences
+    } = req.body;
+
+    const { error } = await supabase
+        .from('profiles')
+        .insert({
+            id: req.user.id,
+            username,
+            avatar,
+            major,
+            year,
+            contact,
+            about,
+            skills,
+            module,
+            experiences
+        });
+
+    if (error) {
+        return res.status(400).json({ error: error.message });
+    }
+    return res.status(200).json({ message: 'Profile created successfully' })
 })
 
 //TODO: Add santization ig? in the future
