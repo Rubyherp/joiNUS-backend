8e7fa956dafe7778a02a89ecb6ac49ddc209e452
XN <horxiangneng123@gmail.com>
2026-05-27 02:11:36 +0800

    add changeAvatar logic

--- server.js
+++ server.js
@@ -2,9 +2,13 @@ import express from 'express';
 import cors from 'cors';
 import { supabase } from './supabaseClient.js';
 import authMiddleware from './middleware/authMiddleware.js';
+import multer from 'multer';
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+const upload = multer({
+    storage: multer.memoryStorage()
+})
 
 app.use(cors());
 app.use(express.json());
@@ -66,14 +70,15 @@ app.post('/login', async (req, res) => {
 
 app.post('/profileCreation', authMiddleware, async (req, res) => {
     const {
-        username,
         avatar,
+        username,
         major,
         year,
+        modules,
         contact,
+        email,
         about,
         skills,
-        module,
         experiences
     } = req.body;
 
@@ -81,15 +86,16 @@ app.post('/profileCreation', authMiddleware, async (req, res) => {
         .from('profiles')
         .insert({
             id: req.user.id,
-            username,
             avatar,
+            username,
             major,
             year,
+            modules,
             contact,
+            email,
             about,
             skills,
-            module,
-            experiences
+            experiences,
         });
 
     if (error) {
@@ -97,6 +103,74 @@ app.post('/profileCreation', authMiddleware, async (req, res) => {
     }
     return res.status(200).json({ message: 'Profile created successfully' })
 })
+
+app.get('/profile', authMiddleware, async (req, res) => {
+    console.log(req.user);
+    const userId = req.user.id;
+    const { data, error } = await supabase
+        .from('profiles')
+        .select('*')
+        .eq('id', userId)
+        .maybeSingle();
+
+    if (error) {
+        return res.status(400).json({ error: error.message });
+    }
+    if (!data) {
+        return res.status(404).json({ error: 'No Profile Found' });
+    }
+
+    return res.status(200).json(data);
+})
+
+app.post('/changeAvatar', authMiddleware, upload.single("avatar"), async (req, res) => {
+    try {
+        console.log("USER:", req.user);
+        console.log("BODY:", req.body);
+        console.log(req.file);
+        const file = req.file;
+
+        if (!file) {
+            return res.status(404).json({ error: 'No file uploaded' });
+        }
+
+        const filePath = `${req.user.id}.jpg`;
+
+        const { error: uploadError } = await supabase.storage
+            .from("avatars")
+            .upload(filePath, file.buffer, {
+                contentType: file.mimetype,
+                upsert: true,
+            });
+
+        if (uploadError) {
+            console.log("STORAGE ERROR:", uploadError);
+            return res.status(400).json({ error: uploadError.message });
+        }
+
+
+        const { data } = supabase.storage
+            .from("avatars")
+            .getPublicUrl(filePath);
+
+        const avatarUrl = data.publicUrl;
+
+        const { error: dbError } = await supabase
+            .from("profiles")
+            .update({ avatar: avatarUrl })
+            .eq('id', req.user.id);
+
+        if (dbError) {
+            return res.status(400).json({ error: dbError.message });
+        }
+
+        return res.status(200).json({ avatar: avatarUrl })
+
+    } catch (error) {
+        return res.status(400).json({ error: error.message });
+    }
+})
+
 
 //TODO: Add santization ig? in the future
 
